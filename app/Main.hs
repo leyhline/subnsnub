4f86e2b71c4e5edfc5bc9789b2df329dc78f36a7
Thomas Leyh <thomas.leyh@mailbox.org>
2022-01-17 22:39:02 +0100

    cmd opts parsing in Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,28 @@
 module Main where
 
-import Subtitle
+import Options.Applicative
+import SilenceDetect
+
+data SilenceDetectOpts = SilenceDetectOpts
+  { path :: String }
+
+silenceDetectOpts :: Parser SilenceDetectOpts
+silenceDetectOpts = SilenceDetectOpts
+  <$> strOption
+    (  long "file"
+    <> metavar "PATH"
+    <> help "path of audio file for silence detection" )
 
 main :: IO ()
-main = return ()
+main = runDetectSilence =<< execParser opts
+  where
+    opts = info (silenceDetectOpts <**> helper)
+      (  fullDesc
+      <> progDesc "Stuff with audio books and subtitles"
+      <> header "subssnub" )
+
+runDetectSilence :: SilenceDetectOpts -> IO ()
+runDetectSilence opts = do
+  intervals <- detectSilence (path opts)
+  let test = map print intervals
+  sequence_ test
