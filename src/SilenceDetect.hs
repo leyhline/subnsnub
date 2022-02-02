15905ae17bb8b071151e7100d85cded0f21c4dc5
Thomas Leyh <thomas.leyh@mailbox.org>
2022-02-02 13:12:50 +0100

    add license header and docstring to SilenceDetect.hs

--- src/SilenceDetect.hs
+++ src/SilenceDetect.hs
@@ -1,6 +1,28 @@
+{-
+subnsnub - helps to create subtitles from and for audiobooks
+Copyright (C) 2022  Thomas Leyh <thomas.leyh@mailbox.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-}
+
+{-|
+Module      : SilenceDetect
+Description : Calls FFmpeg with silencedetect filter and parses output
+-}
 module SilenceDetect
   ( detectSilence
-  , parseSilences
+  , parseSilence
   , SilenceInterval(..)
   ) where
 
@@ -30,11 +52,11 @@ detectSilence d audioFile = do
   (_, _, Just errHdl, p) <- createProcess cmd
   exitCode <- waitForProcess p
   case exitCode of
-    ExitSuccess -> parseSilences . T.pack <$> hGetContents errHdl
+    ExitSuccess -> parseSilence . T.pack <$> hGetContents errHdl
     ExitFailure code -> throw $ ProcessException $ cmdStr ++ " quit with exit code " ++ show code
 
-parseSilences :: Text -> [SilenceInterval]
-parseSilences input = either (throw . ParserException) id (parseOnly silenceParser input)
+parseSilence :: Text -> [SilenceInterval]
+parseSilence input = either (throw . ParserException) id (parseOnly silenceParser input)
 
 silenceParser :: Parser [SilenceInterval]
 silenceParser = many' intervalParser
